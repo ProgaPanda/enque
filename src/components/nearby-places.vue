17cdc2cffe5149781df718ed813ecced71073f1f
Raslan <24810123+aliraslan@users.noreply.github.com>
2019-07-29 21:21:28 +0200

    Adjusted various padding issues

--- src/components/nearby-places.vue
+++ src/components/nearby-places.vue
@@ -2,17 +2,25 @@
   <div>
     <v-list style="background-color:#f3f3f3;">
       <v-container fluid style="height: 60vh; overflow-y: scroll">
-        <v-list-item v-for="place in nearby" :key="place.name">
+        <v-list-item v-for="place in nearby" :key="place.name" class="pa-1">
           <a
             :href="`google.navigation:q=${place.geometry.location.lat()},${place.geometry.location.lng()}`"
             style="text-decoration:none;"
           >
             <v-card style="background-color:#f3f3f3;" flat width="90vw" class="mx-auto my-auto">
-              <h3>{{place.name}}</h3>
-              <v-card-actions style="padding-left:0">
+              <v-card-actions>
                 <v-layout>
-                  <v-icon small left color="#aeaeae">location_on</v-icon>
-                  <div style="color:#757575;padding:0 auto;">2 minutes away.</div>
+                  <v-avatar color="indigo" style="margin-right:15px;">
+                    <v-icon color="white">{{typeIcon(place.types[0])}}</v-icon>
+                  </v-avatar>
+                  <v-flex>
+                    <h3>{{place.name}}</h3>
+                    <v-layout justify-start>
+                      <v-icon small left color="#aeaeae">location_on</v-icon>
+                      <div style="color:#757575;padding:0 auto;">2 minutes away.</div>
+                    </v-layout>
+                  </v-flex>
+
                   <v-spacer></v-spacer>
                   <v-icon right color="blue">open_in_new</v-icon>
                 </v-layout>
@@ -31,6 +39,18 @@ export default {
     this.findNearby();
   },
   methods: {
+    typeIcon(businessType) {
+      switch (businessType) {
+        case "cafe":
+          return "local_cafe";
+        case "supermarket":
+          return "store_mall_directory";
+        case "restaurant":
+          return "restaurant";
+        default:
+          return "place";
+      }
+    },
     async findNearby() {
       const crds = await this.getLocation();
       const fetchPlaces = (businessType, distance = "2000") =>
@@ -50,7 +70,8 @@ export default {
         });
       const places = await Promise.all([
         fetchPlaces("cafe"),
-        fetchPlaces("restaurant")
+        fetchPlaces("restaurant"),
+        fetchPlaces("supermarket")
       ]);
       this.nearby = [...new Set(places.flat())];
       console.log(this.nearby);
