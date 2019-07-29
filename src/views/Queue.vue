09a0ab074d938a81d0f24bb31d282e54db3bc6d6
Raslan <24810123+aliraslan@users.noreply.github.com>
2019-07-29 12:34:39 +0200

    added nearby places

--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <nearby-places />
+  </div>
 </template>
 
 <script>
+import nearbyPlaces from "@/components/nearby-places.vue";
+
 export default {
+  name: "Que",
+  components: {
+    "nearby-places": nearbyPlaces
+  },
   data() {
     return {};
   }
