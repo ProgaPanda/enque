cb9c2283339451f10b4b47157bd4ac09968a4307
Raslan <24810123+aliraslan@users.noreply.github.com>
2019-07-29 20:00:03 +0200

    This is an image

--- src/views/QrScanner.vue
+++ src/views/QrScanner.vue
@@ -1,71 +1,74 @@
 <template>
   <div class="qr-container">
-    <qrcode-stream @decode="onDecode" @init="onInit"/>
+    <qrcode-stream @decode="onDecode" @init="onInit" />
+    <img
+      src="../assets/QR.svg"
+      style="position: absolute; top:25%; bottom:25%; height: 50vh; width: 50vw;"
+    />
   </div>
 </template>
 
 <script>
-    import {QrcodeStream} from 'vue-qrcode-reader'
-    import router from "@/router"
-    export default {
+import { QrcodeStream } from "vue-qrcode-reader";
+import router from "@/router";
+export default {
+  components: { QrcodeStream },
 
-        components: {QrcodeStream},
+  data() {
+    return {
+      result: "",
+      error: ""
+    };
+  },
 
-        data() {
-            return {
-                result: '',
-                error: ''
-            }
-        },
+  methods: {
+    onDecode(result) {
+      console.log(window.location.href, result);
+      router.push("/");
+    },
 
-        methods: {
-            onDecode(result) {
-                console.log(window.location.href, result);
-                router.push("/")
-            },
-
-            async onInit(promise) {
-                try {
-                    await promise
-                } catch (error) {
-                    if (error.name === 'NotAllowedError') {
-                        this.error = "ERROR: you need to grant camera access permisson"
-                    } else if (error.name === 'NotFoundError') {
-                        this.error = "ERROR: no camera on this device"
-                    } else if (error.name === 'NotSupportedError') {
-                        this.error = "ERROR: secure context required (HTTPS, localhost)"
-                    } else if (error.name === 'NotReadableError') {
-                        this.error = "ERROR: is the camera already in use?"
-                    } else if (error.name === 'OverconstrainedError') {
-                        this.error = "ERROR: installed cameras are not suitable"
-                    } else if (error.name === 'StreamApiNotSupportedError') {
-                        this.error = "ERROR: Stream API is not supported in this browser"
-                    }
-                    console.error(this.error);
-                }
-            }
+    async onInit(promise) {
+      try {
+        await promise;
+      } catch (error) {
+        if (error.name === "NotAllowedError") {
+          this.error = "ERROR: you need to grant camera access permisson";
+        } else if (error.name === "NotFoundError") {
+          this.error = "ERROR: no camera on this device";
+        } else if (error.name === "NotSupportedError") {
+          this.error = "ERROR: secure context required (HTTPS, localhost)";
+        } else if (error.name === "NotReadableError") {
+          this.error = "ERROR: is the camera already in use?";
+        } else if (error.name === "OverconstrainedError") {
+          this.error = "ERROR: installed cameras are not suitable";
+        } else if (error.name === "StreamApiNotSupportedError") {
+          this.error = "ERROR: Stream API is not supported in this browser";
         }
-    }
-</script>
-
-<style lang="scss">
-  .qr-container {
-    background: #333;
-    overflow: hidden;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    .wrapper {
-      .inside {
-        video {
-          margin: auto auto;
-          height: 100%;
-          max-width: 100vw;
-        }
-        height: 100vh;
+        console.error(this.error);
       }
     }
   }
+};
+</script>
+
+<style lang="scss">
+.qr-container {
+  background: #333;
+  overflow: hidden;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .wrapper {
+    .inside {
+      video {
+        margin: auto auto;
+        height: 100%;
+        max-width: 100vw;
+      }
+      height: 100vh;
+    }
+  }
+}
 </style>
