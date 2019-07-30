ec0728641826e6b9985132d9a2eabd9b4e227a19
Raslan <24810123+aliraslan@users.noreply.github.com>
2019-07-31 00:20:20 +0200

    Fixed installability

--- src/router.js
+++ src/router.js
@@ -1,28 +1,34 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Queue from './views/Queue';
-import QrScanner from './views/QrScanner';
-import ThankYouScreen from './views/ThankYouScreen';
+import Vue from "vue";
+import Router from "vue-router";
+import Queue from "./views/Queue";
+import QrScanner from "./views/QrScanner";
+import ThankYouScreen from "./views/ThankYouScreen";
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      name: 'queue',
-      path: '/queue',
-      component: Queue,
+      path: "*", // or '/index.html'
+      beforeEnter: (to, from, next) => {
+        next("/");
+      }
     },
     {
-      name: 'scanner',
-      path: '/',
-      component: QrScanner,
+      name: "queue",
+      path: "/queue",
+      component: Queue
     },
     {
-      name: 'thanks',
-      path: '/thanks',
-      component: ThankYouScreen,
+      name: "scanner",
+      path: "/",
+      component: QrScanner
     },
-  ],
+    {
+      name: "thanks",
+      path: "/thanks",
+      component: ThankYouScreen
+    }
+  ]
 });
